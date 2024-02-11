a39b3f76bae08c9e8551bcb9f91e6157b18899b9
akakou <akakou571@gmail.com>
2024-02-11 09:49:38 +0900

    feat: get env vars with default values

---
+++ env.go
@@ -0,0 +1,11 @@
+package golangutils
+
+import "os"
+
+func GetEnv(key string, def string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return def
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akakou/golang-utils
+
+go 1.21.4
